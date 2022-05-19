de76e268e590848e9e0ff35fdbabe8303feac91a
Michelle Sauder <45767844+michellesauder@users.noreply.github.com>
2022-05-19 14:52:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
+require('dotenv').config();
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
-const app = require("express")();
-const httpServer = require("http").createServer(app);
-const cors = require("cors");
-const path = require("path")
-const fs = require("fs");
+const users = {};
 
-const privateKey  = fs.readFileSync('key.pem');
-const certificate = fs.readFileSync('cert.pem');
+const socketToRoom = {};
 
-console.log({certificate})
+io.on('connection', socket => {
+    socket.on("join room", roomID => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
 
-const credentials = { 
-    key: privateKey, 
-    cert: certificate, 
-    requestCert: false, 
-    rejectUnauthorized: false,
-    ca: 'csr.csr'
-};
+        socket.emit("all users", usersInThisRoom);
+    });
 
-const httpsServer = require("https").createServer(credentials, app);
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
 
-const io = require("socket.io")(httpsServer, {
-	cors: {
-		origin: "*",
-		methods: [ "GET", "POST" ]
-	}
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+        }
+    });
+
 });
 
-app.use(cors());
+server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
 
-const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-	res.send('Running');
-});
-
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
-
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	});
-
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
-});
-
-httpsServer.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
